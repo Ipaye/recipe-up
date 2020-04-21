dde09c860be3ccf2db55c0b2fddb416fc653b0a8
Ipaye Alameen <Akinkunmi007@gmail.com>
2020-04-21 23:12:22 +0100

    UI: Create category items

--- lib/widgets/category_items.dart
+++ lib/widgets/category_items.dart
@@ -1,22 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:foodapp/screens/meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
 
-  CategoryItem({this.title, this.color});
+  CategoryItem({this.id, this.title, this.color});
+
+  void selectCategory(BuildContext context) {
+    // Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+    //   return MealScreen(
+    //     categoryId: id,
+    //     categoryTitle: title,
+    //   );
+    // }));
+
+    // todo: Using Named routes
+    Navigator.of(context)
+        .pushNamed(MealScreen.routeName, arguments: {'id': id, 'title': title});
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      child: Text(title),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [color.withOpacity(.7), color],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight),
-          borderRadius: BorderRadius.circular(4)),
+    return InkWell(
+      onTap: () => selectCategory(context),
+      borderRadius: BorderRadius.circular(20),
+      splashColor: Theme.of(context).primaryColor,
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.title,
+        ),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [color.withOpacity(.7), color],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+            borderRadius: BorderRadius.circular(4)),
+      ),
     );
   }
 }
