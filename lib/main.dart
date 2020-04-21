188c5383f60099cb1e757ce33e791596b5d27481
Ipaye Alameen <Akinkunmi007@gmail.com>
2020-04-21 23:13:23 +0100

    Feat: implemented named routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:foodapp/screens/categories_screen.dart';
+import 'package:foodapp/screens/meals_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,11 +11,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Meal Planner',
       theme: ThemeData(
-        fontFamily: 'ZillaSlab',
+        fontFamily: 'Lato',
         primarySwatch: Colors.amber,
         accentColor: Colors.blue,
+        canvasColor: Color.fromRGBO(255, 255, 255, .85),
+        textTheme: ThemeData.light().textTheme.copyWith(
+            body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+            body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+            title: TextStyle(
+                fontSize: 20,
+                fontFamily: 'ZillaSlab',
+                fontWeight: FontWeight.w700)),
       ),
       home: CategoriesScreen(),
+      routes: {MealScreen.routeName: (context) => MealScreen()},
     );
   }
 }
