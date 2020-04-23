836942cd3449b4eab2214b74d5fdf6eb313f521a
Ipaye Alameen <Akinkunmi007@gmail.com>
2020-04-23 09:49:09 +0100

    Refactor: Refactored categories screen to account for tab view.
    Tabs doesnt allow usage of scaffold as it creates that already

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,34 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:foodapp/widgets/category_items.dart';
+import '../widgets/category_items.dart';
 
 import '../mock.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Meal Planner',
-          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-        ),
-      ),
-      body: Container(
-        padding: const EdgeInsets.only(top: 10, left: 8, right: 8, bottom: 10),
-        child: GridView(
-            children: DUMMY_CATEGORIES
-                .map((categoryData) => CategoryItem(
-                      id: categoryData.id,
-                      title: categoryData.title,
-                      color: categoryData.color,
-                    ))
-                .toList(),
-            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 200,
-                childAspectRatio: 3 / 2,
-                crossAxisSpacing: 20,
-                mainAxisSpacing: 20)),
-      ),
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: GridView(
+          children: DUMMY_CATEGORIES
+              .map((categoryData) => CategoryItem(
+                    id: categoryData.id,
+                    title: categoryData.title,
+                    color: categoryData.color,
+                  ))
+              .toList(),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20)),
     );
   }
 }
