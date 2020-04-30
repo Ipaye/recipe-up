6a1b0ce91114cf049f21e99aa20ba6602008ef4c
Ipaye Alameen <Akinkunmi007@gmail.com>
2020-04-30 11:59:29 +0100

    Feat: finished up the tab drawer page

---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import '../screens/filters_screen.dart';
+
+class MainDrawer extends StatelessWidget {
+  Widget buildListTile(String title, IconData icon, Function tapHandler) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontFamily: 'ZillaSlab',
+          fontSize: 20,
+        ),
+      ),
+      onTap: tapHandler,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 120,
+            color: Theme.of(context).accentColor,
+            width: double.infinity,
+            padding: EdgeInsets.only(bottom: 20, left: 15),
+            alignment: Alignment.bottomLeft,
+            child: Text(
+              'Cooking up',
+              style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 30,
+                  fontFamily: 'ZillaSlab',
+                  color: Theme.of(context).primaryColor),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          buildListTile('Meals', Icons.restaurant, () {
+            Navigator.of(context).pushReplacementNamed('/');
+          }),
+          buildListTile('Filters', Icons.settings, () {
+            Navigator.of(context).pushReplacementNamed(FilterScreen.routeName);
+          }),
+        ],
+      ),
+    );
+  }
+}
