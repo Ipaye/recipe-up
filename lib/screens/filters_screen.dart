ed1a6e60b711743a1d7ca9b61fe4e5291cb63791
Ipaye Alameen <Akinkunmi007@gmail.com>
2020-04-30 11:51:37 +0100

    WIP: first iteration at creating the filter page screen

---
+++ lib/screens/filters_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import '../widgets/main_drawer.dart';
+
+class FilterScreen extends StatefulWidget {
+  static const routeName = 'filters';
+
+  @override
+  _FilterScreenState createState() => _FilterScreenState();
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+  var _isGlutenFree = false;
+  var _isVegan = true;
+  var _isVegetarian = true;
+  var _isLactoseFree = true;
+
+  Widget displaySwitchTiles(
+      {bool value, String title, String subtitle, Function onChangeHandler}) {
+    return SwitchListTile(
+        value: value,
+        title: Text(title),
+        subtitle: Text(subtitle),
+        onChanged: onChangeHandler);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Filter Resturant'),
+      ),
+      drawer: MainDrawer(),
+      body: Column(
+        children: <Widget>[
+          Container(
+            child: Text('Filter out what you dont want'),
+          ),
+          Expanded(
+            child: Column(children: <Widget>[
+              displaySwitchTiles(
+                  value: _isGlutenFree,
+                  title: 'Gutten',
+                  subtitle: 'Filter for glutten free meals.',
+                  onChangeHandler: (value) {
+                    setState(() {
+                      value = _isGlutenFree;
+                    });
+                  }),
+              displaySwitchTiles(
+                  value: _isLactoseFree,
+                  title: 'Lactose',
+                  subtitle: 'Filter for lactose free meals.',
+                  onChangeHandler: (value) {
+                    setState(() {
+                      value = _isLactoseFree;
+                    });
+                  }),
+              displaySwitchTiles(
+                  value: _isVegan,
+                  title: 'Gutten',
+                  subtitle: 'Filter for vegan free meals.',
+                  onChangeHandler: (value) {
+                    setState(() {
+                      value = _isVegan;
+                    });
+                  }),
+              displaySwitchTiles(
+                  value: _isVegetarian,
+                  title: 'Gutten',
+                  subtitle: 'Filter for vegetarian free meals.',
+                  onChangeHandler: (value) {
+                    setState(() {
+                      value = _isVegetarian;
+                    });
+                  }),
+            ]),
+          )
+        ],
+      ),
+    );
+  }
+}
